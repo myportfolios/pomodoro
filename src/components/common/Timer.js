88bb4c134e3e025bbd9252a13e6d49ec36dcfa75
Adetola Alexander Agunbiade <30411005+myportfolios@users.noreply.github.com>
2023-01-22 14:27:39 -0500

    Add vibration to Timer

--- src/components/common/Timer.js
+++ src/components/common/Timer.js
@@ -1,22 +1,36 @@
 import React, { useState } from "react"
-import { View, Text, StyleSheet, Button } from "react-native"
+import { View, Text, StyleSheet, Button, Vibration } from "react-native"
 import { ProgressBar } from "react-native-paper"
 import { spacing } from "../../utils/sizes"
 import { colors } from "../../utils/colors"
 import { Countdown } from "./CountDown"
 import { RoundedButton } from "./RoundedButton"
 
+const ONE_SECOND_IN_MS = 1000
+
+const PATTERN = [
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS,
+]
+
 export const Timer = ({ focusItem }) => {
   const [isStarted, setIsStarted] = useState(false)
   const [progress, setProgress] = useState(1)
+  const [minutes, setMinutes] = useState(0.1)
   const isPaused = !isStarted
   return (
     <View style={styles.container}>
       <View style={styles.countdown}>
         <Countdown
+          minutes={minutes}
           isPaused={isPaused}
           onProgress={setProgress}
-          onEnd={() => {}}
+          onEnd={() => {
+            Vibration.vibrate(PATTERN)
+          }}
         />
         <View style={{ paddingTop: spacing.xxl }}>
           <Text style={styles.title}>Focusing on:</Text>
